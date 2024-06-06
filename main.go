27b90d63f7a88bfc97a4b9dcdfd0891bafaf1840
Dowon <ks2515@naver.com>
2024-06-06 11:57:33 +0900

    feat: solved api, send nfty

--- main.go
+++ main.go
@@ -1,76 +1,19 @@
 package main
 
-import (
-	"fmt"
-	"time"
-
-	"github.com/imroc/req/v3"
-)
-
-const SOLVEDAPI string = "https://solved.ac/api/v3"
-
-type UserInfo struct {
-	Handle                  string    `json:"handle"`
-	Bio                     string    `json:"bio"`
-	BadgeID                 string    `json:"badgeId"`
-	BackgroundID            string    `json:"backgroundId"`
-	ProfileImageURL         string    `json:"profileImageUrl"`
-	SolvedCount             int       `json:"solvedCount"`
-	VoteCount               int       `json:"voteCount"`
-	Class                   int       `json:"class"`
-	ClassDecoration         string    `json:"classDecoration"`
-	RivalCount              int       `json:"rivalCount"`
-	ReverseRivalCount       int       `json:"reverseRivalCount"`
-	Tier                    int       `json:"tier"`
-	Rating                  int       `json:"rating"`
-	RatingByProblemsSum     int       `json:"ratingByProblemsSum"`
-	RatingByClass           int       `json:"ratingByClass"`
-	RatingBySolvedCount     int       `json:"ratingBySolvedCount"`
-	RatingByVoteCount       int       `json:"ratingByVoteCount"`
-	ArenaTier               int       `json:"arenaTier"`
-	ArenaRating             int       `json:"arenaRating"`
-	ArenaMaxTier            int       `json:"arenaMaxTier"`
-	ArenaMaxRating          int       `json:"arenaMaxRating"`
-	ArenaCompetedRoundCount int       `json:"arenaCompetedRoundCount"`
-	MaxStreak               int       `json:"maxStreak"`
-	Coins                   int       `json:"coins"`
-	Stardusts               int       `json:"stardusts"`
-	JoinedAt                time.Time `json:"joinedAt"`
-	BannedUntil             time.Time `json:"bannedUntil"`
-	ProUntil                time.Time `json:"proUntil"`
-	Rank                    int       `json:"rank"`
-	IsRival                 bool      `json:"isRival"`
-	IsReverseRival          bool      `json:"isReverseRival"`
-	Blocked                 bool      `json:"blocked"`
-	ReverseBlocked          bool      `json:"reverseBlocked"`
-}
-
-func GetUserInfo(handle string) (*UserInfo, error) {
-	client := req.C()
-	userInfo := UserInfo{}
-	url := fmt.Sprintf("%s/user/show", SOLVEDAPI)
-	resp, err := client.R().
-		SetQueryParam("handle", handle).
-		SetHeader("Content-Type", "application/json").
-		SetHeader("Accept", "application/json").
-		SetSuccessResult(&userInfo).
-		Get(url)
-
-	if err != nil || resp.IsErrorState() {
-		if err == nil {
-			err = fmt.Errorf("status code: %d, error: %v", resp.StatusCode, resp.String())
-		}
-		return nil, err
-	}
-
-	return &userInfo, nil
-}
+import "fmt"
 
 func main() {
-	info, err := GetUserInfo("asdf")
+	// info, err := GetUserInfo("asdf")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// fmt.Printf("%v\n", info)
+
+	err := SendNtfy("", "topic")
+
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Printf("%v\n", info)
 }
---
+++ nfty.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/imroc/req/v3"
+)
+
+const NTFY string = "https://ntfy.sh"
+
+func SendNtfy(baseurl string, topic string) error {
+	if baseurl == "" {
+		baseurl = NTFY
+	}
+
+	if topic == "" {
+		return fmt.Errorf("topic is empty")
+	}
+
+	url, err := url.JoinPath(baseurl, topic)
+	if err != nil {
+		return err
+	}
+
+	client := req.C()
+	resp, err := client.R().
+		SetBody("문제풀어!").
+		SetHeader("Title", "Problem Solving").
+		Post(url)
+
+	if err != nil || resp.IsErrorState() {
+		if err == nil {
+			err = fmt.Errorf("error: %s", resp.String())
+		}
+		return err
+	}
+	return nil
+}
